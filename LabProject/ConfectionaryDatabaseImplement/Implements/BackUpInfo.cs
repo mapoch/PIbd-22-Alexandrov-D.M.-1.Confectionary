3d0ce547c3b598e40592d716dc79b4893d8ac64d
Дмитрий Александров <mapoch2002@yandex.ru>
2022-05-17 22:55:57 +0400

    ю

---
+++ LabProject/ConfectionaryDatabaseImplement/Implements/BackUpInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using ConfectionaryContracts.StoragesContracts;
+
+namespace ConfectionaryDatabaseImplement.Implements
+{
+    public class BackUpInfo : IBackUpInfo
+    {
+        public Assembly GetAssembly() => typeof(BackUpInfo).Assembly;
+        public List<PropertyInfo> GetFullList()
+        {
+            using var context = new ConfectionaryDatabase();
+            var type = context.GetType();
+            return type.GetProperties().Where(x => x.PropertyType.FullName.
+                StartsWith("Microsoft.EntityFrameworkCore.DbSet")).ToList();
+        }
+        public List<T> GetList<T>() where T : class, new()
+        {
+            using var context = new ConfectionaryDatabase();
+            return context.Set<T>().ToList();
+        }
+    }
+}
